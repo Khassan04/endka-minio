260a437fbe0a8bf07375aa8836c8955de541f098
Khassan04 <khassan.turganbekov@narxoz.kz>
2025-12-05 20:50:06 +0500

    second  endpoint

--- src/routes/minio.js
+++ src/routes/minio.js
@@ -5,6 +5,7 @@ const { MinioClient } = require('../config/minio');
 
 const bucket = process.env.MINIO_BUCKET;
 const foldertxt = "Txt-files";
+const folderpng = "Png-files";
 
 router.post('/upload-txt', upload.single('file'), async (req, res) => {
     try {
@@ -23,6 +24,28 @@ router.post('/upload-txt', upload.single('file'), async (req, res) => {
         await MinioClient.putObject(bucket, objectName, file.buffer);
 
         res.json({ message: "Файл .txt успешно загружен в папку Txt-files", filename: file.originalname });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+router.post('/upload-png', upload.single('file'), async (req, res) => {
+    try {
+        const file = req.file;
+
+        if (!file) {
+            return res.status(400).json({ message: "Требуется файл .png" });
+        }
+
+        if (!file.originalname.endsWith(".png")) {
+            return res.status(400).json({ message: "Разрешены только .png файлы" });
+        }
+
+        const objectName = `${folderpng}/${file.originalname}`;
+
+        await MinioClient.putObject(bucket, objectName, file.buffer);
+
+        res.json({ message: "Файл .png успешно загружен в папку Png-files", filename: file.originalname });
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
